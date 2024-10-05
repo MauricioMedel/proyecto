a12a558548d53d4d8ee5a000f6b0005974ff2feb
miguelbv28 <brionesvmiguel@gmail.com>
2024-10-05 10:31:43 -0600

    feat: diseño registro producto

--- src/app/registro-producto/registro-producto.page.ts
+++ src/app/registro-producto/registro-producto.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro-producto',
@@ -7,9 +9,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroProductoPage implements OnInit {
 
-  constructor() { }
+  nombreProducto: string = '';
+  cantidad: number | null = null;
+  precioCompra: number | null = null;
+  precioVenta: number | null = null;
+  formSubmitted: boolean = false;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private alertController: AlertController
+  ) { }
+
+  ngOnInit() { }
+
+  async cancelRegistration() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar',
+      message: '¿Seguro que quieres cancelar?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+        {
+          text: 'Sí',
+          handler: () => {
+            this.limpiarFormulario();
+            this.router.navigate(['/productos']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
+  async registrarProducto() {
+    this.formSubmitted = true;
+    if (!this.validarFormulario()) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, completa todos los campos obligatorios.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: 'Registro de producto realizado correctamente.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+    this.limpiarFormulario();
+    this.router.navigate(['/productos']);
+  }
+
+  limpiarFormulario() {
+    this.nombreProducto = '';
+    this.cantidad = null;
+    this.precioCompra = null;
+    this.precioVenta = null;
+    this.formSubmitted = false;
+  }
+
+  validarFormulario() {
+    return this.nombreProducto && this.cantidad !== null && 
+           this.precioCompra !== null && this.precioVenta !== null;
+  }
 }
