9f1b8b71f3db12c51caf3b0bd36a83893a816890
UbaldoFloresR <20223l001019@utcv.edu.mx>
2024-10-05 11:30:03 -0600

    feat: Correción en almacen

--- src/app/almacen/almacen.page.ts
+++ src/app/almacen/almacen.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'; // Importa NavController
 
 @Component({
   selector: 'app-almacen',
   templateUrl: './almacen.page.html',
   styleUrls: ['./almacen.page.scss'],
 })
-export class AlmacenPage implements OnInit {
+export class AlmacenPage {
+  // Definición de la propiedad productos
+  productos = [
+    { nombre: 'Paracetamol', categoria: 'Medicamento', precio: 100, stock: 50, descripcion: 'Alivia el dolor y reduce la fiebre.' },
+    { nombre: 'Ibuprofeno', categoria: 'Medicamento', precio: 150, stock: 40, descripcion: 'Utilizado comúnmente para reducir la inflamación.' },
+    // Añade más productos según sea necesario
+  ];
 
-  constructor() { }
+  // Inyecta NavController en el constructor
+  constructor(private navCtrl: NavController) { }
 
-  ngOnInit() {
+  // Método para regresar a la página de productos
+  regresar() {
+    this.navCtrl.navigateBack('/productos'); // Utiliza navigateBack para regresar a la página de productos
   }
-
 }
