65b1628929ee3aae75908d21d94fc31d88b70eaf
David-F-P <davidbv645@gmail.com>
2024-10-12 16:56:29 -0600

    fix: historial de ventas

--- src/app/ventas/ventas.page.ts
+++ src/app/ventas/ventas.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.page.html',
   styleUrls: ['./ventas.page.scss'],
 })
-export class VentasPage implements OnInit {
+export class VentasPage {
 
   constructor() { }
 
-  ngOnInit() {
+  // Acción para registrar una nueva venta
+  registrarVenta() {
+    // Aquí puedes redirigir a la página de registro de venta o abrir un formulario modal
+    console.log('Registrando nueva venta...');
+  }
+
+  // Acción para ver el historial de ventas
+  verHistorialVentas() {
+    // Aquí puedes redirigir a la página de historial de ventas o cargar los datos
+    console.log('Viendo historial de ventas...');
   }
 
 }
