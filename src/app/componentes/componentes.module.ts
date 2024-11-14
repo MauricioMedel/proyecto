88137ed21dcee4343620d730edbeea10ed881da0
miguelbv28 <brionesvmiguel@gmail.com>
2024-11-13 20:49:57 -0600

    feat: creacion componente

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { BotonComponent } from './boton/boton.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BotonComponent],
   imports: [
-    CommonModule
+    CommonModule, IonicModule
+  ],
+  exports:[
+    BotonComponent
   ]
 })
 export class ComponentesModule { }
